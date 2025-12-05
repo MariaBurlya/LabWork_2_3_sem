2182280b3680d1ae8a45d471cf5d06091d42f2b4
Marrrrrusya <166085136+MariaBurlya@users.noreply.github.com>
2025-12-05 20:49:26 +0700

    Update main.cpp
    cmd работает с русским текстом

--- main.cpp
+++ main.cpp
@@ -10,17 +10,20 @@
 
 using namespace std;
 
+// Компаратор для сортировки по убыванию
 struct Compare {
     bool operator()(const int& a, const int& b) const {
-        return a > b;
+        return a > b; // сортировка по убыванию длины строк
     }
 };
 
 class TextFileProcessor {
 private:
+    // Контейнер: ключ - длина строки, значение - позиция в файле
     multimap<int, int, Compare> linesData;
     
 public:
+    // Чтение файла и заполнение контейнера
     void readAndStoreFile(const string& inputFilename) {
         ifstream inputFile(inputFilename, ios::binary);
         
@@ -31,17 +34,17 @@ public:
         }
         
         string currentLine;
-        streampos linePosition = inputFile.tellg();
-        int lineCounter = 0;
+        streampos linePosition = inputFile.tellg(); // текущая позиция в файле
         
         while (getline(inputFile, currentLine)) {
             int lineLength = currentLine.length();
             int position = static_cast<int>(linePosition);
             
+            // Сохраняем длину строки и её позицию в файле
             linesData.insert(make_pair(lineLength, position));
             
+            // Получаем позицию для следующей строки
             linePosition = inputFile.tellg();
-            lineCounter++;
         }
         
         inputFile.close();
@@ -51,6 +54,7 @@ public:
         }
     }
     
+    // Запись отфильтрованных строк в выходной файл
     void writeFilteredOutput(const string& inputFilename, const string& outputFilename) {
         ifstream inputFile(inputFilename, ios::binary);
         if (!inputFile.is_open()) {
@@ -62,18 +66,26 @@ public:
             throw runtime_error("Не удалось создать файл '" + outputFilename + "' для записи");
         }
         
+        // Используем lower_bound и upper_bound для поиска диапазона строк
+        // lower_bound(20) - первая строка с длиной <= 20 (из-за сортировки по убыванию)
         auto rangeBegin = linesData.lower_bound(20);
+        
+        // upper_bound(10) - первая строка с длиной < 10
         auto rangeEnd = linesData.upper_bound(10);
         
+        // Перебираем строки в найденном диапазоне
         for (auto it = rangeBegin; it != rangeEnd; ++it) {
-            int lineLength = it->first;
-            int filePosition = it->second;
+            int lineLength = it->first;      // длина строки
+            int filePosition = it->second;   // позиция в файле
             
+            // Дополнительная проверка длины (10-20 символов)
             if (lineLength >= 10 && lineLength <= 20) {
+                // Переходим к позиции строки в исходном файле
                 inputFile.seekg(filePosition);
                 
                 string lineContent;
                 if (getline(inputFile, lineContent)) {
+                    // Записываем строку в выходной файл
                     outputFile << lineContent << '\n';
                 }
             }
@@ -84,12 +96,14 @@ public:
     }
 };
 
+// Функция для вывода инструкции по использованию
 void printUsage(const char* programName) {
     cout << endl;
     cout << "Необходимо ввести: " << programName << " \"входной_файл.txt\" \"выходной_файл.txt\"" << endl;
-    }
+}
 
 int main(int argc, char* argv[]) {
+ 
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
@@ -100,14 +114,15 @@ int main(int argc, char* argv[]) {
     }
     
     string inputFile = argv[1];
-    string outputFile = argv[2];
+    string outputFile = argv[2];  
     
     try {
         TextFileProcessor processor;
-        processor.readAndStoreFile(inputFile);
-        processor.writeFilteredOutput(inputFile, outputFile);
+        processor.readAndStoreFile(inputFile);       // читаем и обрабатываем файл
+        processor.writeFilteredOutput(inputFile, outputFile); // записываем результат
         
     } catch (const exception& e) {
+        // Обработка исключений
         cerr << "\nОшибка: " << e.what() << endl;
         return 1;
     }
